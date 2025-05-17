e6906fa78eed71f8d4c6afdb99ff086818145bff
ANIRUDH <anirudhnarang0@gmail.com>
2025-05-18 01:07:00 +0530

    added code for creating form and getting all the forms and also by thier id

--- action/form.ts
+++ action/form.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import prisma from "@/lib/prisma";
+import { formSchema, formSchemaType } from "@/schemas/form";
 import { currentUser } from "@clerk/nextjs/server";
 
 class UserNotFoundErr extends Error{
@@ -37,4 +38,62 @@ export async function GetFormStats() {
     return {
         visits,submissions,submissionRate,bounceRate
     }
+}
+
+
+
+export async function CreateForm(data: formSchemaType) {
+    const validation = formSchema.safeParse(data);
+    if (!validation.success) {
+      throw new Error("Validation failed");
+    }
+    
+    const user = await currentUser();
+    if (!user) {
+      throw new UserNotFoundErr();
+    }
+    
+    const form = await prisma.form.create({
+      data: {
+        name: data.name,
+        description: data.description ?? "",
+        userID: user.id,
+      }
+    });
+    
+    if (!form) {
+      throw new Error("Form creation failed");
+    }
+    
+    return form.id;
+  }
+
+
+export async function GetForms (){
+  const user=await currentUser();
+  if (!user){
+    throw new UserNotFoundErr();
+  }
+  return await prisma.form.findMany({
+    where:{
+      userID:user.id,
+    },
+    orderBy:{
+      createdAt:"desc",
+    },
+  }
+
+  )
+}
+
+export async function GetFormById(id: number) {
+  const user=await currentUser();
+  if (!user){
+    throw new UserNotFoundErr();
+  }
+  return await prisma.form.findUnique({
+    where:{
+      id,
+    },
+  })
 }
