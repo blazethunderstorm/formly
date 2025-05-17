0441d0585edb46760ab4d96dcc8d3b45fc9f62a8
ANIRUDH <anirudhnarang0@gmail.com>
2025-05-18 01:10:15 +0530

    feat: add Prisma client singleton to prevent multiple instances

--- lib/generated/prisma/index.d.ts
+++ lib/generated/prisma/index.d.ts
@@ -3410,6 +3410,7 @@ export namespace Prisma {
 
   export type FormWhereUniqueInput = Prisma.AtLeast<{
     id?: number
+    name_userID?: FormNameUserIDCompoundUniqueInput
     AND?: FormWhereInput | FormWhereInput[]
     OR?: FormWhereInput[]
     NOT?: FormWhereInput | FormWhereInput[]
@@ -3423,7 +3424,7 @@ export namespace Prisma {
     submissions?: IntFilter<"Form"> | number
     shareURL?: StringFilter<"Form"> | string
     FormSubmissions?: FormSubmissionListRelationFilter
-  }, "id">
+  }, "id" | "name_userID">
 
   export type FormOrderByWithAggregationInput = {
     id?: SortOrder
@@ -3698,6 +3699,11 @@ export namespace Prisma {
 
   export type FormSubmissionOrderByRelationAggregateInput = {
     _count?: SortOrder
+  }
+
+  export type FormNameUserIDCompoundUniqueInput = {
+    name: string
+    userID: string
   }
 
   export type FormCountOrderByAggregateInput = {
