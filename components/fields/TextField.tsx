871247f0cb5952235d51c601e2e446862ee0bd87
ANIRUDH <anirudhnarang0@gmail.com>
2025-05-25 21:28:12 +0530

    made changes in TextField

--- components/fields/TextField.tsx
+++ components/fields/TextField.tsx
@@ -2,28 +2,52 @@
 
 import { MdTextFields } from "react-icons/md";
 import React from "react";
-import {ElementType, FormElement} from "../FormElements";
+import {ElementsType, FormElementInstance, FormElements} from "../FormElements";
+import { Label } from "recharts";
+import { Input } from "../ui/input";
 
-const type:ElementType="TextField";
-
-export const TextFieldElement:FormElement={
+const type:ElementsType="TextField";
+const extraAttributes={
+    label:"TextField",
+    helperText:"Helper Text",
+    required:false,
+    placeHolder:"Value Here ..."
+}
+export const TextFieldElement:FormElements={
 
     type,
     construct:(id:string) =>({
         id,
         type,
-        extraAttributes:{
-            label:"TextField",
-            helperText:"HelperText",
-            required:false,
-            placeHolder:"Value Here ...",
-        }
+        extraAttributes,
     }),
     designerBtnElement:{
-        icon: React.createElement(MdTextFields),
+        icon: MdTextFields,
         label:"TextField",
     },
-    designerComponent:() => <div>Designer Component</div>,
+    designerComponent: DesignerComponent,
     formComponent:()=><div>Form Component</div>,
     propertiesComponent:()=><div>Properties Component</div>,
 };
+
+
+type CustomInstance=FormElementInstance & {
+    extraAttributes:typeof extraAttributes
+}
+
+function DesignerComponent({elementInstance,}:{elementInstance:FormElementInstance}){
+    const element=elementInstance as CustomInstance;
+    const{label,required,placeHolder,helperText}=element.extraAttributes;
+    return ( 
+    <div className="flex flex-col gap-2 w-full">
+        <Label>
+            {label}
+            {required && "*"}
+        </Label>
+        <Input readOnly disabled placeholder={placeHolder}/>
+        {helperText && (
+            <p className="text-muted-foreground text-[0.8rem]">{helperText}</p>
+        )}
+    </div>
+    );
+}
